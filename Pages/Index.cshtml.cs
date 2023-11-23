e38802954e548b7b9e7f5515122ba8c0a983b73b
Northerner988 <lulu12100@hotmail.co.uk>
2023-11-23 07:28:39 +0000

    correct onGetAsync to use ToDoTasks

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+using ToDoListApp.Data;
+
+
 
 namespace ToDoList.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+   private readonly ToDoListApp.Data.ToDoListContext _context;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ToDoListApp.Data.ToDoListContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public void OnGet()
-    {
+    public IList<ToDoTask> ToDoTask { get;set; } = default!;
 
+    public async Task OnGetAsync()
+    {
+        ToDoTask = await _context.ToDoTasks.ToListAsync();
     }
 }
+
+
+   
+
+
+
+//     namespace ToDoList.Pages;
+
+// public class IndexModel : PageModel
+// {
+//     private readonly ILogger<IndexModel> _logger;
+
+//     public IndexModel(ILogger<IndexModel> logger)
+//     {
+//         _logger = logger;
+//     }
+
+//     public void OnGet()
+//     {
+
+//     }
+// }
