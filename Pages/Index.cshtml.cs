86177922052210318618f91b9f3537d77da39744
Northerner988 <lulu12100@hotmail.co.uk>
2023-11-23 08:25:34 +0000

    show the added tasks in the homepage

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -32,21 +31,3 @@ public class IndexModel : PageModel
 
    
 
-
-
-//     namespace ToDoList.Pages;
-
-// public class IndexModel : PageModel
-// {
-//     private readonly ILogger<IndexModel> _logger;
-
-//     public IndexModel(ILogger<IndexModel> logger)
-//     {
-//         _logger = logger;
-//     }
-
-//     public void OnGet()
-//     {
-
-//     }
-// }
