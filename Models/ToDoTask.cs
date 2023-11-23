49f695d2ebc547b9014e9a62ecda3f23c4213e58
Northerner988 <lulu12100@hotmail.co.uk>
2023-11-23 07:26:47 +0000

    add validation

--- Models/ToDoTask.cs
+++ Models/ToDoTask.cs
@@ -1,14 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ToDoListApp.Models
+namespace ToDoList.Models
 {
     public class ToDoTask
-    {
-        public int Id { get; set; }
+   {
+      public int Id { get; set; }
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Please enter a valid title.")]
+        [Required,StringLength(50, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 50 characters.")]
         public string Title { get; set; } = string.Empty;
+
+     
+        [Required, StringLength(300, MinimumLength=3 ,ErrorMessage = "Description must be between 3 and 300 characters.")]
         public string Description { get; set; } = string.Empty;
 
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Please select a date.")]
         public DateTime SelectDate { get; set; }
-    }
+}
 }
