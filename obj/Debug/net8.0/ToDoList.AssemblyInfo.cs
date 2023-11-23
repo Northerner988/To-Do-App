8ada08e857c9b003edb49fab7486e656f5c98208
Northerner988 <lulu12100@hotmail.co.uk>
2023-11-23 02:17:09 +0000

    intiate EF core migration

--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e6291c6f69c85bfa37623a35e3d2554f3ac062")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
